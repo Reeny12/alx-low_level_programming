37f172f3a3c30831897669575358853169d6680c
Reeny12 <lebotsamaureen@gmail.com>
2023-03-16 09:37:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using the print function
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
