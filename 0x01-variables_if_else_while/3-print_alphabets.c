85ddf7a8c244a0bc6ccb2891eee9dab2e02a1a77
Reeny12 <lebotsamaureen@gmail.com>
2023-03-17 11:09:05 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-	char alph[26] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26; i++)
