a009aae1f38b9490841bd67987e0beec17412766
Reeny12 <lebotsamaureen@gmail.com>
2023-03-17 11:13:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - checks if a number is positive,zero or negative
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
