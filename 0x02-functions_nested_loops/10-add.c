00ea29b0ca472dc32788fd043a659531bc757717
Reeny12 <lebotsamaureen@gmail.com>
2023-03-21 12:13:33 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @n1: Returns the sign of a number
+ * @n2: Returns the sign of a number
+ * Return: 1 , 0 or -1
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
