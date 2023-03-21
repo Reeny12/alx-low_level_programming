0aed06603cbe472c421e0d1c06dec25ee8855cdc
Reeny12 <lebotsamaureen@gmail.com>
2023-03-21 05:25:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - checks if a number is positive,zero or negative
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
