69b6bc42f1537dbbdd2cd4627c0b0f1ac4826725
Reeny12 <lebotsamaureen@gmail.com>
2023-03-23 05:22:28 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - checks if a letter is an uppercase
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar("%c", c);
+	}
+	_putchar('\n');
+}
