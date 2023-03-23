b7784fc3b7b53fcc10866668c18ee6d446b3ac1d
Reeny12 <lebotsamaureen@gmail.com>
2023-03-23 07:56:51 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - checks if a letter is an uppercase
+ *
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(x + '48');
+		}
+	}
+	putchar('\n');
+}
