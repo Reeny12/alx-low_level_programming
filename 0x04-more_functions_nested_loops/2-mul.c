60b2aaf96cf8504f975215bc293730902170efc6
Reeny12 <lebotsamaureen@gmail.com>
2023-03-23 05:14:04 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - checks if a letter is an uppercase
+ * @a: letter to be checked
+ * @b: letter to check
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
