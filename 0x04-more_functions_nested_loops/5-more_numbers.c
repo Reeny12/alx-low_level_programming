c19b380ab27c26c9d6154cff1eff629ddac440fa
Reeny12 <lebotsamaureen@gmail.com>
2023-03-23 11:33:58 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - checks if a letter is an uppercase
+ *
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 0; a < 14; a++)
+		{
+			putchar(a + 'a');
+		}
+		putchar('\n');
+	}
+}
