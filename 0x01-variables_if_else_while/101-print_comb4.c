650133db9fa90099fdc499501a19037b072977e4
Reeny12 <lebotsamaureen@gmail.com>
2023-04-02 14:35:43 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,21 @@
  */
 int main(void)
 {
-	int d, p, q;
+	int a, b, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (a = '0'; a < '9'; a++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (b = a + 1; b <= '9'; b++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (q = b + 1; q <= '9'; q++)
 			{
-				if ((p != d) != q)
-				putchar(d);
+				if ((b != a) != q)
+				putchar(a);
 				putchar(p);
 				putchar(q);
-				if  (d == '7' && p == '8')
+				if  (a == '7' && b == '8')
 				continue;
-				putchar(',');
+				putchar('44');
 				putchar(' ');
 			}
 		}
