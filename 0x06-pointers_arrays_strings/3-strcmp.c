b99b248c62a41a033608a402b613b78836ea1689
Reeny12 <lebotsamaureen@gmail.com>
2023-04-02 16:26:00 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != 0)
-	{
+	do {
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
 		i++;
-	}
+	} while (s1[i] != '\0' && s2[i] != 0);
 	return (0);
 }
