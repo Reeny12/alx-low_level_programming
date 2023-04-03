2c8625d5009df42adbce2736d64c8f3aebe5fd77
Reeny12 <lebotsamaureen@gmail.com>
2023-04-03 10:08:32 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - fill a block of memory with b
+ * @s: the adress of memory to be filled
+ * @accept: the character to fill the memory with
+ * Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = 0, counter = 0;
+	int n;
+
+	while (s[a] != '\0')
+	{
+		for (n = 0; accept[n]; n++)
+		{
+			if (accept[n] == s[a])
+			{
+				counter++;
+				break;
+			}
+			else if (accept[n] == '\0')
+			{
+				return (counter);
+			}
+		}
+		a++;
+	}
+	return (counter);
+}
