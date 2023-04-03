47608e9b4eb0ef35474811ebfc7b7fbca131a66d
Reeny12 <lebotsamaureen@gmail.com>
2023-04-03 16:59:57 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - fill a block of memory with b
+ * @a: the adress of memory to be filled
+ * Return: s
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int v = 0, n;
+
+	while (v < 8)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[v][n]);
+		}
+		putchar ('\0');
+	v++;
+	}
+}
