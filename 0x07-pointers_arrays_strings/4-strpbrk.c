a82ee5b0373a2b0f32b040cffa63f764e9318126
Reeny12 <lebotsamaureen@gmail.com>
2023-04-03 16:23:03 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - fill a block of memory with b
+ * @s: the adress of memory to be filled
+ * @accept: the character to fill the memory with
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0;
+
+	do {
+		for (; accept[a]; k++)
+		{
+			if (*s == accept[a])
+			{
+				return (s);
+			}
+		}
+		s++;
+	} while(*s);
+	return ('\0');
+}
