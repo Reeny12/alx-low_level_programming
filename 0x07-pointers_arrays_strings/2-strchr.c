7792274cf934efc66c184fa35ef6aa62af106f9f
Reeny12 <lebotsamaureen@gmail.com>
2023-04-03 06:58:31 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
  * _strchr - fill a block of memory with b
  * @s: the adress of memory to be filled
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	int a = 0;
 
-	for (; s[a] >= '\0'; i++)
+	for (; s[a] >= '\0'; a++)
 	{
 		if (s[a] == c)
 		{
-			return (s + i);
+			return (s + a);
 		}
 	}
 	return (NULL);
