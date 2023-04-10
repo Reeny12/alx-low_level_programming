212efa92adb3fbec50466081c8d5370a71b7b943
Reeny12 <lebotsamaureen@gmail.com>
2023-04-10 13:37:13 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
+	int a = 0, a = 0;
 
 	if (argc == 3)
 	{
-		n1 == atoi(argv[1]);
-		n2 == atoi(argv[2]);
-		printf("%d\n", n1 * n2);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
 	}
 	else
 	{
