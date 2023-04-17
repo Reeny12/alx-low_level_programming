b4da78815928c3038a03a7cf41760fe583da9103
Reeny12 <lebotsamaureen@gmail.com>
2023-04-17 14:17:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ struct dog
  * init_dog - typedef for struct dog
  */
 
-typedef struct init_dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
