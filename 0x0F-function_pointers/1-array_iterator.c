b031c04c696d47a895c9cf2913301bc6830577fc
Reeny12 <lebotsamaureen@gmail.com>
2023-04-19 02:41:27 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - ...
+ * @array: ...
+ * @size: ...
+ * @action: ...
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
