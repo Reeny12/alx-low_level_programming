677372b0a9fbe0617e25ebc6f3ce93ddb12a0a08
Reeny12 <lebotsamaureen@gmail.com>
2023-06-16 10:44:43 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,21 @@
 #include <stddef.h>
 #include "lists.h"
 
-size_t dlistint_len(const dlistint_t *h) {
-    size_t count = 0;
+/**
+ * dlistint_len - ...
+ * @h: ...
+ * Return: count
+ */
 
-    while (h != NULL) {
-        count++;
-        h = h->next;
-    }
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
 
-    return count;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
