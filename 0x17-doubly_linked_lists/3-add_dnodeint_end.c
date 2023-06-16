b30102480ee4f7181f1d26b039a49e1191d4f668
Reeny12 <lebotsamaureen@gmail.com>
2023-06-16 10:10:36 -0700

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n) {
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL) {
+        return NULL;  // Memory allocation failed
+    }
+
+    new_node->n = n;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        new_node->prev = NULL;
+        *head = new_node;
+    } else {
+        dlistint_t *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+        new_node->prev = temp;
+    }
+
+    return new_node;
+}
