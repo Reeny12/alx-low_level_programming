1d9a203e4cbde8020c0aa9e3f59afac4e2a42f2a
Reeny12 <lebotsamaureen@gmail.com>
2023-06-18 06:20:30 -0700

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - ...
+ * @head: ...
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
